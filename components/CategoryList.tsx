9336a3c83cbc111351e9653af50dbdc540643363
Denis <draghicidenis@yahoo.com>
2024-11-15 19:48:42 +0200

    fixed image

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -11,7 +11,7 @@ const CategoryList: React.FC<CategoryListProps> = ({ categories }) => (
     <div className="categories-list flex gap-4 overflow-x-auto">
       {categories.map((category) => (
         <div key={category.category_id} className="category-item text-center">
-          <img src="../assets/boots.jpg" alt={category.category_name} className="w-16 h-16" />
+          <img src={category.image} alt={category.category_name} className="w-16 h-16" />
           <span>{category.category_name}</span>
         </div>
       ))}
