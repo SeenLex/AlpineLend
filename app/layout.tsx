6e68477f0d8dff4228e067f21860c3a7f5ffb33e
Denis <draghicidenis@yahoo.com>
2025-01-15 23:54:37 +0200

    testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,112 @@
-"use client";
+// "use client";
 
-import "@/assets/globals.css";
+// import "@/assets/globals.css";
+// import localFont from "next/font/local";
+// import Footer from "@/components/Footer";
+// import { usePathname } from "next/navigation";
+
+// const geistSans = localFont({
+//   src: "../assets/fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "../assets/fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   const pathname = usePathname();
+//   const noFooterPaths = ["/", "/login", "/register", "/error"];
+
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//           {!noFooterPaths.includes(pathname) && <Footer />}
+//       </body>
+//     </html>
+//   );
+// }
 import localFont from "next/font/local";
-import Footer from "@/components/Footer";
-import { usePathname } from "next/navigation";
+import "./globals.css";
 
 const geistSans = localFont({
-  src: "../assets/fonts/GeistVF.woff",
+  src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "../assets/fonts/GeistMonoVF.woff",
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+import type { Metadata, Viewport } from "next";
+
+const APP_NAME = "AlpineLend";
+const APP_DEFAULT_TITLE = "AlpineLend";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-  const noFooterPaths = ["/", "/login", "/register", "/error"];
-
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-          {!noFooterPaths.includes(pathname) && <Footer />}
       </body>
     </html>
   );
