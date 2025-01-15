04c38267a603f882d9f7ec109065fb877e84fa5e
Denis <draghicidenis@yahoo.com>
2025-01-15 22:16:10 +0200

    prisma edited

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,36 +6,27 @@ import CategoryList from "@/components/CategoryList";
 import PopularSlider from "@/components/PopularItems";
 import SearchableCategoryList from "@/components/SearchableCategoryList";
 
-import Navbar from "@/components/Navbar";
-
 
 const HomePage = async () => {
   const categories = await getAllCategories();
   const popularItems = await getPopularItems();
-        
+
   return (
     <div className="min-h-screen bg-gray-50">
-      {/* Header */}
-
       <header className="fixed top-0 left-0 w-full bg-white shadow-md z-10">
         <div className="px-4 py-3">
           <h1 className="text-lg font-semibold text-gray-800">AlpineLend</h1>
         </div>
 
         <div className="px-4 pb-3">
-        <section>
-        {/* Searchable Category List */}
-        <SearchableCategoryList categories={categories} />
-        </section>
+          <section>
+            <SearchableCategoryList categories={categories} />
+          </section>
         </div>
       </header>
 
-      <Navbar />
-
-
       {/* Main Content */}
       <main className="pt-36 pb-20 p-4 sm:p-6">
-
         <div className="max-w-screen-lg mx-auto space-y-12">
           <CategoryList categories={categories} />
 
@@ -70,7 +61,8 @@ const HomePage = async () => {
           </section>
         </div>
       </main>
-    </div>  );
+    </div>
+  );
 };
 
 export default HomePage;
